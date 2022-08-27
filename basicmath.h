3a1b9c4484fac0d215b5aa02b0b5a63f4e07fa7a
Tanaji Langote <111754771+mr-langote@users.noreply.github.com>
2022-08-28 00:39:11 +0530

    28-08-2022

--- basicmath.h
+++ basicmath.h
@@ -1,1 +1,18 @@
+/* A basic math functions header file in c 
+as First year mini project*/
+#include<math.h>
+int fact(int x){
+  if (x==1)
+    return 1;
+  return (x*fact(x-1));
+}
+int getsum(int x){
+  if (x==0)
+    return 0;
+  return (x+getsum(x-1));
+}
+int cube(int x){
+  return x*x*x;
+}
+
 
